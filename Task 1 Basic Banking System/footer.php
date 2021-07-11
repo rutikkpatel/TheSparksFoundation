88d95fadb136fdce4b821ebcfc03936806d1c5ed
Rutik K Patel <72887393+rutikkpatel@users.noreply.github.com>
2021-07-11 17:41:33 +0530

    Add files via upload

---
+++ Task 1 Basic Banking System/footer.php
@@ -0,0 +1,6 @@
+
+<!-- Footer Part -->
+    <footer>
+        <h5> Task-1 Basic Banking System &#9997; Designed By : <a href="https://rutikkpatel.github.io/Portfolio1/">Rutik Patel</a></h5>
+    </footer>
+<!-- END OF Footer -->
---
+++ Task 1 Basic Banking System/nav.php
@@ -0,0 +1,57 @@
+<!-- nav section starts here -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CK Bank | Task 1 </title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel = "icon" href = "img/logo.png" type = "image/x-icon">
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+  <!-- Popper JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</head>
+
+
+<body>
+
+ <!-- NAVBAR START -->
+  <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="#"> <img src="img/logo.png" height="70px" width="70px"> </a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item active">
+                <a class="nav-link mr-5 px-3" href="index.html">Home </a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link mr-5 px-3" href="#aboutme">About Me </a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link mr-5 px-3" href="#skills">Contact Me </a>
+            </li>
+      </ul>
+    </div>
+  </nav>
+
+<!-- END OF NAVBAR -->
